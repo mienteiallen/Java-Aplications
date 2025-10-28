6d093bcb8bc9b79154b4168d2353554979020c53
Tariemi Peregba <snowallen6@gmail.com>
2025-10-28 10:43:31 +0100

    data type

---
+++ OperatorsPart1.java
@@ -0,0 +1,16 @@
+public class OperatorsPart1{
+	public static void main(String[] args){
+		int num = 50;
+		num += 8;
+		System.out.printf("The valus of num is %d%n",num);
+		num -= 7;
+		System.out.printf("The valus of num is %d%n",num);
+		num *= 10;
+		System.out.printf("The valus of num is %d%n",num);
+		num /= 5;
+		System.out.printf("The valus of num is %d%n",num);
+		num %= 3;
+		System.out.printf("The valus of num is %d%n",num);
+	}
+
+}
---
+++ UserInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserInput{
+	public static voi main(String[] args){
+		Scanner input = new Scanner(System.in)
+		
+		
+		System.out.print("Enter your first name: ")
+		Sting firstName = input.nextLine();
+		
+		System.out.print("Enter your first name: ")
+	}
+
+}
